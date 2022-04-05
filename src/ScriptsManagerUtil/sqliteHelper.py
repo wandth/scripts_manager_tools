b443911356ae7b4f2044a776728b802775ff0ffe
wwn <1506514799@qq.com>
2022-04-05 20:02:20 +0800

    bug fix for update tag scripts

--- src/ScriptsManagerUtil/sqliteHelper.py
+++ src/ScriptsManagerUtil/sqliteHelper.py
@@ -1,15 +1,9 @@
 # coding=utf-8
 import json
-import os
 import sqlite3
-
-from PySide2.QtCore import QDir, QFileInfo, QTextCodec
-import json
-import os
-import sqlite3
-
 from maya import cmds
 from contextlib import closing
+from PySide2.QtCore import QDir, QFileInfo
 
 
 class SqliteHelper :
@@ -57,54 +51,13 @@ class SqliteHelper :
             if root_child.isDir() and root_child.baseName() in self.default_tags.keys() :
                 for child in QDir(root_child.absoluteFilePath()).entryInfoList(QDir.NoSymLinks | QDir.NoDotAndDotDot | QDir.Files | QDir.Dirs) :  # type: QFileInfo
                     if child.isDir() :
-                        new_folder = QDir(child.absoluteFilePath())
-                        child_files = new_folder.entryInfoList(QDir.NoSymLinks | QDir.NoDotAndDotDot | QDir.Files | QDir.Dirs)
-                        if len(child_files) != 2 and "scripts" not in [x.fileName() for x in child_files] :
-                            cmds.error("{} is not a valid script folder".format(child.absoluteFilePath()))
-                            continue
-                        script_infos = [p for p in child_files if p.isFile() and p.completeSuffix() == "py"]
-                        if not script_infos :
-                            cmds.error("not has a scripts to startup {folder}".format(folder = new_folder.absoluteFilePath()))
-                            continue
-                        self.__updateScript(name = script_infos[0].baseName(),
-                                            script_path = script_infos[0].absoluteFilePath(),
-                                            script_type = "python", python_type = "module", tag = root_child.baseName())
+                        self.__updateFolder(child, tag = root_child.baseName())
                     elif child.isFile() :
-                        if child.completeSuffix() == "py" :
-                            self.__updateScript(name = child.baseName(), script_path = child.absoluteFilePath(), script_type = "python", python_type = "script",
-                                                tag = root_child.baseName())
-                        elif child.completeSuffix() == "mel" :
-                            self.__updateScript(name = child.baseName(), script_path = child.absoluteFilePath(), script_type = "mel", python_type = "",
-                                                tag = root_child.baseName())
+                        self.__updateFile(child, tag = root_child.baseName())
             elif root_child.isDir() and root_child.baseName() not in self.default_tags.keys() :
-                pass
+                self.__updateFolder(root_child, tag = "")
             if root_child.isFile() :
-                if root_child.completeSuffix() == "py" :
-                    self.__updateScript(name = root_child.baseName(), script_path = root_child.absoluteFilePath(), script_type = "python", python_type = "script", tag = "")
-                elif root_child.completeSuffix() == "mel" :
-                    self.__updateScript(name = root_child.baseName(), script_path = root_child.absoluteFilePath(), script_type = "mel", python_type = "", tag = "")
-
-    def __updateScript(self, name, script_path, script_type, python_type, tag) :
-        with closing(sqlite3.connect(self.db_path)) as database :
-            database.text_factory = bytes
-            cursor = database.cursor()
-
-            cursor.execute(U"""SELECT * FROM scripts WHERE name = "{name}" """.format(name = name))
-            if cursor.fetchall() :
-                update_str = U"""UPDATE scripts SET script_path = "{script_path}", type = "{script_type}", python_type = "{python_type}" WHERE name = "{name}" """.format(
-                    script_path = script_path, script_type = script_type, python_type = python_type, name = name)
-                cursor.execute(update_str)
-            else :
-                insert_str = U"""INSERT INTO scripts(name, script_path, type, python_type) VALUES("{name}", "{script_path}", "{script_type}", "{python_type}")""".format(
-                    name = name, script_path = script_path, script_type = script_type, python_type = python_type)
-                cursor.execute(insert_str)
-
-
-            database.commit()
-
-        if tag != "" :
-            self.addScriptTag(script_name = name, tag_name = tag)
-
+                self.__updateFile(root_child, tag = "")
 
     def addScriptTag(self, script_name, tag_name) :
         with closing(sqlite3.connect(self.db_path)) as database :
@@ -129,4 +82,41 @@ class SqliteHelper :
     def addTag(self, tag_name) :
         pass
 
+    def __updateFolder(self, file_ino, tag) :
+        new_folder = QDir(file_ino.absoluteFilePath())
+        child_files = new_folder.entryInfoList(QDir.NoSymLinks | QDir.NoDotAndDotDot | QDir.Files | QDir.Dirs)
+        if len(child_files) != 2 and "scripts" not in [x.fileName() for x in child_files] :
+            cmds.error("{} is not a valid script folder".format(file_ino.absoluteFilePath()))
+            return
+        script_infos = [p for p in child_files if p.isFile() and p.completeSuffix() == "py"]
+        if not script_infos :
+            cmds.error("not has a scripts to startup {folder}".format(folder = new_folder.absoluteFilePath()))
+            return
+        self.__updateScript(name = script_infos[0].baseName(),
+                            script_path = script_infos[0].absoluteFilePath(),
+                            script_type = "python", python_type = "module", tag = tag)
 
+    def __updateFile(self, file_ino, tag) :
+        if file_ino.completeSuffix() == "py" :
+            self.__updateScript(name = file_ino.baseName(), script_path = file_ino.absoluteFilePath(), script_type = "python", python_type = "script", tag = tag)
+        elif file_ino.completeSuffix() == "mel" :
+            self.__updateScript(name = file_ino.baseName(), script_path = file_ino.absoluteFilePath(), script_type = "mel", python_type = "", tag = tag)
+
+    def __updateScript(self, name, script_path, script_type, python_type, tag) :
+        with closing(sqlite3.connect(self.db_path)) as database :
+            database.text_factory = bytes
+            cursor = database.cursor()
+
+            cursor.execute(U"""SELECT * FROM scripts WHERE name = "{name}" """.format(name = name))
+            if cursor.fetchall() :
+                update_str = U"""UPDATE scripts SET script_path = "{script_path}", type = "{script_type}", python_type = "{python_type}" WHERE name = "{name}" """.format(
+                    script_path = script_path, script_type = script_type, python_type = python_type, name = name)
+                cursor.execute(update_str)
+            else :
+                insert_str = U"""INSERT INTO scripts(name, script_path, type, python_type) VALUES("{name}", "{script_path}", "{script_type}", "{python_type}")""".format(
+                    name = name, script_path = script_path, script_type = script_type, python_type = python_type)
+                cursor.execute(insert_str)
+            database.commit()
+
+        if tag != "" :
+            self.addScriptTag(script_name = name, tag_name = tag)

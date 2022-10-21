a5f721cd2c4020cf2efd9aa0be50c767d018714c
wwn <1506514799@qq.com>
2022-10-21 22:13:53 +0800

    脚本类别页面添加搜索功能

--- src/scripts_manager_tool/ScriptsManagerUI/scriptsCollectionWidget.py
+++ src/scripts_manager_tool/ScriptsManagerUI/scriptsCollectionWidget.py
@@ -2,10 +2,11 @@
 from PySide2.QtCore import Qt
 from PySide2.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QTreeView
 
-from scripts_manager_tool.ScriptsManagerUI.modelView import scriptsTreeModel
+from scripts_manager_tool.ScriptsManagerUI.modelView import scriptsTreeModel, scriptsCollectionFilterModel
 from scripts_manager_tool.ScriptsManagerUI.util import scriptManagerModel as sql
 
 reload(scriptsTreeModel)
+reload(scriptsCollectionFilterModel)
 reload(sql)
 
 
@@ -13,29 +14,30 @@ class ScriptsCollectionWidget(QWidget):
     def __init__(self, parent):
         super(ScriptsCollectionWidget, self).__init__(parent)
         
-        # self.sql.updateScripts()
-        # self.sql.addCollection("cfx")
-        # self.sql.addScriptToCollection("cfx", u"测试")
-        # self.sql.removeScriptFromCollection("cfx", u"测试")
-        
         main_layout = QVBoxLayout(self)
         
-        self.search_lineedit = QLineEdit()
+        self.search_line = QLineEdit()
         
-        self.script_view = QTreeView(self)
-        self.script_view.setHeaderHidden(True)
-        self.script_view.setFocusPolicy(Qt.NoFocus)
-        self.script_view.setSelectionMode(QTreeView.SingleSelection)
+        self.scripts_collection_view = QTreeView(self)
+        self.scripts_collection_view.setHeaderHidden(True)
+        self.scripts_collection_view.setFocusPolicy(Qt.NoFocus)
+        self.scripts_collection_view.setSelectionMode(QTreeView.SingleSelection)
         
-        self.script_model = scriptsTreeModel.ScriptsTreeModel()
+        self.script_source_model = scriptsTreeModel.ScriptsTreeModel()
         
-        main_layout.addWidget(self.search_lineedit)
-        main_layout.addWidget(self.script_view)
+        self.updateModel()
         
-        self.initModel()
+        self.script_filter_model = scriptsCollectionFilterModel.ScriptsCollectionFilterModel()
+        self.script_filter_model.setSourceModel(self.script_source_model)
+        self.scripts_collection_view.setModel(self.script_filter_model)
+        
+        main_layout.addWidget(self.search_line)
+        main_layout.addWidget(self.scripts_collection_view)
+        
+        self.search_line.textChanged.connect(self.search)
     
-    def initModel(self):
-        root = self.script_model.root()
+    def updateModel(self):
+        root = self.script_source_model.root()
         for collection in sql.ScriptsCollection.getScriptsCollections():
             current_collection_item = scriptsTreeModel.TreeItem(root)
             current_collection_item.setPtr(collection)
@@ -47,5 +49,6 @@ class ScriptsCollectionWidget(QWidget):
                 current_script_item.setPtr(script)
                 current_script_item.setLevel(2)
                 current_collection_item.appendChild(current_script_item)
-        
-        self.script_view.setModel(self.script_model)
+    
+    def search(self):
+        self.script_filter_model.setFilterWildcard(self.search_line.text())
